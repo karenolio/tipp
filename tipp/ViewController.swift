f388936d238735fdf9be8f9476980c315947d46a
Karen Ng <karenkn52@gmail.com>
2017-02-19 13:52:13 -0800

    added some new things

--- tipp/ViewController.swift
+++ tipp/ViewController.swift
@@ -1,4 +1,3 @@
-//
 //  ViewController.swift
 //  tipp
 //
@@ -9,17 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
 
+    @IBOutlet weak var billName: UILabel!
+    @IBOutlet weak var tipName: UILabel!
+    @IBOutlet weak var totalName: UILabel!
+    @IBOutlet weak var dollarSign: UILabel!
+    
+    // Do any additional setup after loading the view, typically from a nib.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.billField.becomeFirstResponder()
+        
+        self.billField.font = UIFont(name: "Futura", size: 32)
+        self.dollarSign.font = UIFont(name: "Futura", size: 32)
+        self.billName.font = UIFont(name: "Futura", size: 12)
+        self.totalLabel.font = UIFont(name: "Futura", size: 32)
+        self.totalName.font = UIFont(name: "Futura", size: 12)
+        self.tipLabel.font = UIFont(name: "Futura", size: 32)
+        self.tipName.font = UIFont(name: "Futura", size: 12)
+        
+        self.tipName.alpha = 0
+        self.tipLabel.alpha = 0
+        self.totalName.alpha = 0
+        self.totalLabel.alpha = 0
+        self.tipControl.alpha = 0
     }
 
+    // Dispose of any resources that can be recreated.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    //tap anywhere outside Bill textfield and it minimizes keypad
+    @IBAction func onTap(_sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_sender: AnyObject) {
+        
+        
+        let tipPercentages = [0.18, 0.20, 0.25]
+        let bill = Double(billField.text!) ?? 0 //take string in billField.text and change into a number and if someone pastes something that's a nil (not a valid #), return 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        self.tipName.alpha = 1
+        self.tipLabel.alpha = 1
+        self.totalName.alpha = 1
+        self.totalLabel.alpha = 1
+        self.tipControl.alpha = 1
+ 
+    }
+    
 }
 
